2e5a598a891e0e5b73c513337dea0ac0348678c0
Filip Ulmer <filipulmer01@gmail.com>
2024-02-23 21:19:56 +0100

    add Entities Author.cs Book.cs Category.cs

---
+++ LibraryAPI/LibraryAPI/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace LibraryAPI.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int PageCount { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public int AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+
+        public virtual List<Category> Categories { get; set; }
+    }
+}
